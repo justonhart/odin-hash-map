352711eafa3d689f5ac5d0432a302eb743b2db8d
Juston Hart <contact@justonh.art>
2024-07-30 13:00:06 -0500

    Fully implement HashMap with Jest tests

--- src/HashMap.ts
+++ src/HashMap.ts
@@ -7,52 +7,135 @@ export default class HashMap {
 	constructor() {
 		this.capacity = 16;
 		this.buckets = [];
+		this.createBuckets();
 	}
 
+	/**
+	 * Populate the buckets array with new LinkedLists
+	 */
+	private createBuckets(): void {
+		for(let i = 0; i < this.capacity; i++) {
+			this.buckets[i] = new LinkedList();
+		}
+	}
+
+	/**
+	 * Compute the hash for a given key to decide which bucket the node belongs in
+	 */
 	private hash(key: string): number {
 		let hashCode: number = 0;
 		const primeNumber = 31;
 		for (let i = 0; i < key.length; i++) {
 			hashCode = primeNumber * hashCode + key.charCodeAt(i);
 		}
-		return hashCode;
+		return hashCode % this.capacity;
+	}
+	
+	/**
+	 * Gets the number of non-empty buckets. Used for calculating load and determining when to resize
+	 */
+	private getNonEmptyBucketCount(): number {
+		return this.buckets.reduce((sum, next) => (sum + next.size() > 0) ? 1 : 0, 0);
 	}
 
 	/**
 	 * A helper function to mock index out-of-bounds behavior in less dynamic languages, as described in the prompt
 	 */
-	private getBucket(index: number) {
+	private getBucket(index: number): LinkedList {
 		if(index < 0 || index >= this.buckets.length) {
 			throw new Error("Trying to access index out of bound");
 		}
 		return this.buckets[index];
 	}
 
+	/**
+	 * Resize the buckets array, doubling capacity and reducing load. Replace all old values at their new locations
+	 */
+	private resize(): void {
+		const oldBuckets = this.buckets;
+		this.capacity *= 2;
+		this.createBuckets();
+
+		oldBuckets.forEach(bucket => {
+			bucket.getNodesAsArray().forEach(node => {
+				this.set(node.key, node.value)
+			})
+		});
+	}
+
+	/**
+	 * Create the provided key-value pair in the hashmap
+	 */
 	public set(key: string, value: string): void {
-		
+		let keyhash = this.hash(key);
+		if(this.getBucket(keyhash).containsKey(key)){
+			let index = this.getBucket(keyhash).getNodeByKey(key)
+			this.getBucket(keyhash).removeNodeAtIndex(index);
+		} 
+
+		this.getBucket(keyhash).append(key, value);
+
+		if(this.length() > this.capacity * this.LOAD_FACTOR){
+			console.log(`After setting ${key} at length ${this.length()}, resizing`);
+			this.resize();
+		}
 	}
 
-	public get(key:string): string {
-		return;
+	/**
+	 * Returns the value associated to provided key. If not found, returns null
+	 */
+	public get(key:string): string | null {
+		let keyhash = this.hash(key);
+		let nodeIndex = this.getBucket(keyhash).getNodeByKey(key);
+		return this.getBucket(keyhash).at(nodeIndex).value;
 	}
 
+	/**
+	 * Returns true if any bucket contains the provided key, otherwise false
+	 */
 	public has(key: string): boolean {
-		return;
+		let keyhash = this.hash(key);
+		return this.getBucket(keyhash).containsKey(key);
 	}
 
+	/**
+	 * If a node with the provided key exists, removes that node and returns true; otherwise false
+	 */
 	public remove(key: string): boolean {
-		return;
+		let keyhash = this.hash(key);
+		let bucket = this.getBucket(keyhash);
+		if(bucket.containsKey(key)) {
+			let nodeIndex = bucket.getNodeByKey(key);
+			bucket.removeNodeAtIndex(nodeIndex);
+			return true;
+		} else {
+			return false;
+		}
 	}
 
+	/**
+	 * Returns the number of stored key-value pairs
+	 */
 	public length(): number {
-		return;
+		let count = 0;
+		this.buckets.forEach(bucket => {
+			count += bucket.size();
+		});
+		return count;
 	}
 
+	/**
+	 * Removes all key-value pairs and resizes the bucket array back to 16
+	 */
 	public clear(): void {
 		this.capacity = 16;
 		this.buckets = [];
+		this.createBuckets();
 	}
 
+	/**
+	 * Returns all keys in the hashmap in an array
+	 */
 	public keys(): string[] {
 		let keys = [];
 		this.buckets.forEach(bucket => 
@@ -61,6 +144,9 @@ export default class HashMap {
 		return keys;
 	}
 
+	/**
+	 * Returns all values in the hashmap in an array
+	 */
 	public values(): string[] {
 		let values = [];
 		this.buckets.forEach(bucket => 
@@ -69,6 +155,9 @@ export default class HashMap {
 		return values;
 	}
 
+	/**
+	 * Returns all key-value pairs in the hashmap as an array of pair arrays
+	 */
 	public entries(): string[][] {
 		let entries = [];
 
@@ -77,5 +166,9 @@ export default class HashMap {
 		});
 
 		return entries;
+	}
+
+	public getLoadRatio(): number {
+		return this.length() / this.capacity;
 	}
 }
--- tests/hashmap.test.ts
+++ tests/hashmap.test.ts
@@ -4,8 +4,73 @@ let map: HashMap;
 
 beforeEach(() => {
 	map = new HashMap();
+	map.set('one', '1');
+	map.set('two', '2');
+	map.set('three', '3');
+	map.set('four', '4');
+	map.set('five', '5');
 });
 
-test('test hashmap clear', () => {
-	expect(true).toBeTruthy();
+test('verify that set overwrites existing keys', () => {
+	map.set('one', 'ONE');
+	expect(map.get('one')).toBe('ONE');
+});
+
+test('test get', () => {
+	expect(map.get('three')).toBe('3');
+	expect(map.get('six')).toBeNull;
+	map.set('six', '6');
+	expect(map.get('six')).toBe('6');
+});
+
+test('test has', () => {
+	expect(map.has('three')).toBe(true);
+	expect(map.has('six')).toBe(false);
+	map.set('six', '6');
+	expect(map.has('six')).toBe(true);
+});
+
+test('test remove', () => {
+	expect(map.has('three')).toBe(true);
+	map.remove('three');
+	expect(map.has('three')).toBe(false);
+});
+
+test('test length', () => {
+	expect(map.length()).toBe(5);
+});
+
+test('test clear', () => {
+	map.clear();
+	expect(map.length()).toBe(0);
+});
+
+test('project-defined test script', () => {
+	
+	//clear out the predefined values
+	map.clear();
+	 
+	map.set('apple', 'red');
+	map.set('banana', 'yellow');
+	map.set('carrot', 'orange');
+	map.set('dog', 'brown');
+	map.set('elephant', 'gray');
+	map.set('frog', 'green');
+	map.set('grape', 'purple');
+	map.set('hat', 'black');
+	map.set('ice cream', 'white');
+	map.set('jacket', 'blue');
+	map.set('kite', 'pink');
+	map.set('lion', 'golden');
+
+	expect(map.getLoadRatio()).toBe(0.75);
+
+	map.set('lion', 'cat');
+	map.set('kite', 'small');
+	map.set('ice cream', 'vanilla');
+
+	expect(map.getLoadRatio()).toBe(0.75);
+
+	map.set('moon', 'silver');
+	expect(map.getLoadRatio()).toBeLessThan(0.75);
 });
